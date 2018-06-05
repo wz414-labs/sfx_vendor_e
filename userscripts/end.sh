bd0c0aed8acc31e409def0fbcb2fcbb49645cb6d
Romain Hunault <romain.hunault@eelo.io>
2018-06-05 10:58:22 +0200

    Revert "Add begin and end scripts"
    This reverts commit 40c5cdfc1421950baef9e7a943c7f2d7e4b8f4e8.

--- userscripts/end.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Publish packages
-/usr/bin/rsync -avz /srv/zips/ root@images.eelo.io:/eelo/builds/full/
-/usr/bin/rsync -avz /srv/zips/ root@ota.eelo.io:/mnt/docker/ota/builds/full/

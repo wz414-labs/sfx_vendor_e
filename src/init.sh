6a2b805ffafd81229dce0f1b2d2c715ad64e343d
steadfasterX <steadfasterX@gmail.com>
2020-07-14 20:11:03 +0200

    add debug output
    Signed-off-by: steadfasterX <steadfasterX@gmail.com>

--- src/init.sh
+++ src/init.sh
@@ -18,8 +18,10 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # check required variables
+echo ">> [$(date)] DEBUG: ROOT_DIR is set to: >$ROOT_DIR<"
 if [ -z "$ROOT_DIR" ]||[ ! -d "$ROOT_DIR" ];then
-    echo ">> [$(date)] ERROR: Please set the ROOT_DIR (e.g. ROOT_DIR=./root) variable and ensure it is an existing directory" && exit 3
+    echo ">> [$(date)] ERROR: Please set the ROOT_DIR (e.g. ROOT_DIR=./root) variable and ensure it is an existing directory"
+    exit 3
 fi
 
 # Copy the user scripts

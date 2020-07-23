11f23d978e2ff27a4a99f1cd453adc10253d2e06
steadfasterX <steadfasterX@gmail.com>
2020-07-23 11:29:32 +0200

    mk: fix deps, replace mka targets
    this introduces 2 new targets which replacing the old ones.
    builde and dirtye are not working anymore.
    instead:
    
     - mka eos (will do all steps, including sync)
     - mka eos-nosync (like eos, just without sync)
    
    The makefile now also ensures the proper dependencies and so processing
    all tasks in correct order.
    
    Signed-off-by: steadfasterX <steadfasterX@gmail.com>

--- build/core/config.mk
+++ build/core/config.mk
@@ -1,23 +1,32 @@
+# understanding PHONY and deps:
+# https://www.gnu.org/software/make/manual/html_node/Phony-Targets.html
+
 # mka builde will run preparee bacon and then finalizee
-builde: preparee synce postsynce bacon finalizee
+eos: preparee synce postsynce bacon finalizee
 .PHONY: builde
 
 # dirty build target without syncing the sources
-dirtye: preparee postsynce bacon finalizee
-.PHONY: dirtye
+eos-nosync: preparee postsync-nosync bacon finalize-nosync
+.PHONY: eos-nosync
 
 # prepare & init e
 preparee:
 	$(BASH) vendor/e/src/init.sh
 
 # force sync everything needed
-synce:
+synce: preparee
 	$(BASH) vendor/e/sync.sh
 
 # special handling after sync, before actually build
-postsynce:
+postsynce: preparee synce
+	$(BASH) vendor/e/post-sync.sh
+
+postsync-nosync: preparee
 	$(BASH) vendor/e/post-sync.sh
 
 # final tasks after building e
-finalizee:
+finalizee: preparee postsynce bacon
+	$(BASH) vendor/e/finalize.sh
+
+finalize-nosync: preparee postsync-nosync bacon
 	$(BASH) vendor/e/finalize.sh
